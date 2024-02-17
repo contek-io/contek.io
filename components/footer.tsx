bb779cfbf99670089af7f96316ef6ebc174a271a
AlexanderZhao <zhaomengzhe@gmail.com>
2024-02-17 20:21:06 +0800

    feat(): dev page

---
+++ components/footer.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+export const Footer = () => {
+  return (
+    <footer className="flex justify-center">
+      <div className="w-full max-w-[1440px] px-16 py-8">
+        <div></div>
+        <div className="flex justify-between text-sm">
+          <div>© 2024 Contek, Ltd All rights reserved</div>
+          <div className="flex gap-4">
+            <a>Privacy</a>
+            <a>Terms of use</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
---
+++ components/header.tsx
@@ -0,0 +1,17 @@
+import Image from "next/image";
+
+export const Header = () => {
+  return (
+    <header className="flex justify-center">
+      <div className="w-full max-w-[1440px] flex justify-between px-16 py-8">
+        <Image src="/logo.svg" alt="contek logo" width={132} height={26} />
+        <div className="flex gap-8 font-medium text-lg text-[#a9acac]">
+          <nav>About us</nav>
+          <nav>Career</nav>
+          <nav>Contact</nav>
+          <nav>LinkedIn</nav>
+        </div>
+      </div>
+    </header>
+  );
+};
