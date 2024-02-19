199bdf3bccdd9b84810186646fbc21f791bb904e
AlexanderZhao <zhaomengzhe@gmail.com>
2024-02-19 11:38:52 +0800

    feat(): set content max width

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Footer = () => {
   return (
     <footer className="flex justify-center">
-      <div className="w-full max-w-[1440px] px-16 py-8">
+      <div className="w-full max-w-[--contek-max-width] px-16 py-8">
         <div></div>
         <div className="flex justify-between text-sm">
           <div>© 2024 Contek, Ltd All rights reserved</div>
