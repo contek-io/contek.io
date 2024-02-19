939b6c798557ec247aa177bf01fd264f5f27e35f
AlexanderZhao <zhaomengzhe@gmail.com>
2024-02-19 15:16:35 +0800

    feat(): add links

--- app/page.tsx
+++ app/page.tsx
@@ -29,40 +29,61 @@ const Home = () => (
         </div>
       </div>
     </div>
-    <div className="flex justify-center bg-[#ffffff]">
+    <div className="flex justify-center bg-[#ffffff]" id="about-us">
       <div className="max-w-[--contek-max-width] h-[680px] flex gap-24 px-16 text-[#111111]">
-        <div className="w-1/2">
+        <div className="w-1/2 flex justify-center items-center">
           <Image src="/about.png" alt="contek logo" width={752} height={680} />
         </div>
-        <div className="w-1/2 flex flex-col justify-center">
+        <div className="w-1/2 flex flex-col justify-center gap-4">
           <p className="font-semibold text-[#1EA665] tracking-wider">ABOUT</p>
-          <p className="">
+          <p className="font-medium text-[42px] leading-[52px]">
             We are a{" "}
             <span className="text-[#1EA665]">proprietary trading firm</span>,
             leveraging on technology to provide liquidity to the market.
           </p>
-          <p className="">
+          <p className="text-lg text-[#656F6E]">
             Our scientists bring rigorous inquiry, data analysis, and invention
             to solve the toughest challenges and build up our own trading
             strategies.
           </p>
-          <p className="">
+          <p className="text-lg text-[#656F6E]">
             Our trading is based on our own models, using our own capital and
             done at our own risk.
           </p>
         </div>
       </div>
     </div>
-    <div className="flex justify-center">
-      <div className="w-full max-w-[608px] h-[680px] flex flex-col justify-center items-center text-center">
-        <p className="">JOIN US</p>
-        <p className="">Strength in numbers</p>
-        <p className="">
+    <div className="relative flex justify-center">
+      <div className="w-full max-w-[608px] h-[680px] flex flex-col gap-8 justify-center items-center text-center overflow-hidden">
+        <div className="absolute -z-[1] h-full left-0">
+          <iframe
+            allowFullScreen
+            className="border-none"
+            height="680"
+            src="https://rive.app/s/iExBZC5AD0eYrA1Lcrq-Sg/embed"
+            width="520"
+          />
+        </div>
+        <div className="absolute -z-[1] h-full right-0">
+          <iframe
+            allowFullScreen
+            className="border-none"
+            height="680"
+            src="https://rive.app/s/66ulUv9FeEmdjtx74YK9Rg/embed"
+            width="520"
+          />
+        </div>
+        <p className="font-semibold text-[#1EA665] tracking-wider">JOIN US</p>
+        <p className="font-medium text-[64px] leading-[70px]">
+          Strength in numbers
+        </p>
+        <p className="text-lg text-[#A9ACAC]">
           We employ motivated and highly talented individuals who want to work
           at the leading edge of investment management in an open and supportive
           environment. Our firm&apos;s culture is characterised by
           collaboration, intellectual integrity, and methodological rigour.
         </p>
+        <Button>Working at Contek</Button>
       </div>
     </div>
     <div className="flex justify-center bg-[#ffffff]">
@@ -72,10 +93,12 @@ const Home = () => (
         <Career />
       </div>
     </div>
-    <div className="flex justify-center">
+    <div className="flex justify-center" id="contact">
       <div className="w-full max-w-[--contek-max-width] h-[500px] flex flex-col justify-center items-center px-16 py-8">
-        <p className="">Get in touch</p>
-        <p className="">contact@contek.io</p>
+        <p className="font-medium text-[28px] leading-[34px]">Get in touch</p>
+        <p className="font-medium text-[104px] leading-[128px] text-[#1EA665]">
+          contact@contek.io
+        </p>
       </div>
     </div>
   </main>
--- components/header.tsx
+++ components/header.tsx
@@ -1,16 +1,29 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export const Header = () => {
   return (
     <header className="flex justify-center">
       <div className="w-full max-w-[--contek-max-width] flex justify-between px-16 py-8">
         <Image src="/logo.svg" alt="contek logo" width={132} height={26} />
-        <div className="flex gap-8 font-medium text-lg text-[#A9ACAC]">
-          <nav>About us</nav>
-          <nav>Career</nav>
-          <nav>Contact</nav>
-          <nav>LinkedIn</nav>
-        </div>
+        <nav className="flex gap-8 font-medium text-lg text-[#A9ACAC] transition">
+          <Link className="hover:opacity-80" href="#about-us">
+            About us
+          </Link>
+          <Link className="hover:opacity-80" href="#career">
+            Career
+          </Link>
+          <Link className="hover:opacity-80" href="#contact">
+            Contact
+          </Link>
+          <Link
+            className="hover:opacity-80"
+            href="https://www.linkedin.com/company/contek/"
+            target="_blank"
+          >
+            LinkedIn
+          </Link>
+        </nav>
       </div>
     </header>
   );
