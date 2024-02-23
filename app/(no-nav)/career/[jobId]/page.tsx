b4897b09430d7029c5baa99dd4ed4008a0e88b36
AlexanderZhao <zhaomengzhe@gmail.com>
2024-02-23 21:18:21 +0800

    feat(): add application google form link

--- app/(no-nav)/career/[jobId]/page.tsx
+++ app/(no-nav)/career/[jobId]/page.tsx
@@ -1,8 +1,11 @@
-import type { Metadata } from 'next';
+import { type Metadata } from 'next';
+import Link from 'next/link';
 import Markdown from 'react-markdown';
 
+import { Button } from '@/components/button';
 import { JobsResponse } from '@/components/career/types';
 import { APITABLE_RECORD_URL } from '@/consts/url';
+import { APPLY_FORM_URL } from '@/consts/url';
 
 type Props = {
   params: { jobId: string };
@@ -43,8 +46,13 @@ const JobDetails = async ({ params }: Props) => {
 
     return (
       <div className="w-full bg-white">
-        <div className="max-w-[--contek-max-width] min-h-[calc(100vh-92px)] mx-auto px-16 py-8 prose">
-          <Markdown>{job?.fields?.requirements}</Markdown>
+        <div className="max-w-[--contek-max-width] min-h-[calc(100vh-92px)] mx-auto px-16 py-8">
+          <div className="max-w-full mb-4 prose">
+            <Markdown>{job?.fields?.requirements}</Markdown>
+          </div>
+          <Link className="hover:opacity-80" href={APPLY_FORM_URL} target="_blank">
+            <Button>Apply</Button>
+          </Link>
         </div>
       </div>
     );
