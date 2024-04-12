9a36e219a3dd6c11455e070b05cd732b815ddd9d
AlexanderZhao <zhaomengzhe@gmail.com>
2024-04-12 15:58:13 +0800

    feat(): dev mobile styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import flowbite from 'flowbite-react/tailwind';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
@@ -5,6 +6,7 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    flowbite.content(),
   ],
   theme: {
     extend: {},
@@ -16,6 +18,6 @@ const config: Config = {
     },
   },
   darkMode: 'class',
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), flowbite.plugin()],
 };
 export default config;
